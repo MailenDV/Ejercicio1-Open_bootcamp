e8398b958dfd77a5fd592dc3e63a1608ea952974
Mailen Di Vito Bottan <mailendivito@gmail.com>
2023-04-27 21:56:54 -0300

    entrega ejercicio primer tema

---
+++ src/Coche.java
@@ -0,0 +1,17 @@
+public class Coche {
+    public static void main(String[] args) {
+        int entero = 10;
+        double decimal = 24.52;
+        String caracter = "a";
+        boolean booleano = true;
+
+        System.out.println("el valor del entero es " + entero);
+
+        System.out.println("el valor del decimal es " + decimal);
+
+        System.out.println("el valor del caracter es " + caracter);
+
+        System.out.println("el valor del booleano es " + booleano);
+
+    }
+}
